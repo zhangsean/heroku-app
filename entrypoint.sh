b964f79fc0699dcf2b8bb1684bdad323bcbf78f2
zhangsean <zxf2342@qq.com>
2021-01-19 23:03:59 +0800

    Add br

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sed -i "s|80;|$PORT;|g" /etc/nginx/conf.d/default.conf
-sed -i "s|nginx!|nginx! Deploy by $NAME!|g" /usr/share/nginx/html/index.html
+sed -i "s|nginx!|nginx! <br/>Deploy by $NAME!|g" /usr/share/nginx/html/index.html
 nginx -g "daemon off;"
